c631c93e36877f3d4658bb667ceaed24d83b4270
Hyeo Min Park <hyeominpark@Hyeos-MacBook-Air.local>
2023-12-14 16:18:29 +0900

    redux-thunk done

--- src/redux/config/configStore.ts
+++ src/redux/config/configStore.ts
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
 import todos from "../modules/todos";
 
 const store = configureStore({
@@ -6,3 +7,9 @@ const store = configureStore({
 });
 
 export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
